75b618c08fd6098cfba76bd14ddea0b205be5e28
shinyoungyou <austyn.you@gmail.com>
2024-11-10 21:22:53 -0800

    Conditional display on mygroups page and new groups page

--- scripts/groups.js
+++ scripts/groups.js
@@ -5,7 +5,6 @@ function displayMyGroupsDynamically(collection) {
     let groupTemplate = document.getElementById("groupListTemplate"); // Retrieve the HTML element with the ID "groupTemplate" and store it in the cardTemplate variable.
     let newGroupTemplate = document.getElementById("newGroupListTemplate"); // Retrieve the HTML element with the ID "groupTemplate" and store it in the cardTemplate variable.
 
-
     db.collection(collection)
         .get() //the collection called "Groups"
         .then((allSubjects) => {
@@ -17,23 +16,33 @@ function displayMyGroupsDynamically(collection) {
                 var created_by = doc.data().created_by; // get value of the "created_by" key
                 var is_my_group = doc.data().is_my_group; // get value of the "is_my_group" key
 
-                if (is_my_group) {
-                    let newList = groupTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                let newList = groupTemplate?.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
+                let newGroupList = newGroupTemplate?.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
 
-                    newList.querySelector("#groupName").innerHTML = subject_name;
+                if (is_my_group && newList) {
+                    newList.querySelector("#groupName").innerHTML =
+                        subject_name;
                     let NumberOfMembersAndCreatedBy = `${number_of_members}/50 person • ${created_by}`;
-                    newList.querySelector("#groupNumberOfMembersAndCreatedBy").innerHTML = NumberOfMembersAndCreatedBy;
+                    newList.querySelector(
+                        "#groupNumberOfMembersAndCreatedBy"
+                    ).innerHTML = NumberOfMembersAndCreatedBy;
 
-                    document.getElementById(collection + "-go-here").appendChild(newList);
-                } else {
-                    // show this dagta on new groupos
-                    let newGroupList = newGroupTemplate.content.cloneNode(true); // Clone the HTML template to create a new card (newcard) that will be filled with Firestore data.
-
-                    newList.querySelector("#groupName").innerHTML = subject_name;
+                    document
+                        .getElementById(collection + "-go-here")
+                        .appendChild(newList);
+                } else if (!is_my_group && newGroupList) {
+                    // show this data on new groupos
+                    console.log(newGroupList);
+                    newGroupList.querySelector("#groupName").innerHTML =
+                        subject_name;
                     let NumberOfMembersAndCreatedBy = `${number_of_members}/50 person • ${created_by}`;
-                    newList.querySelector("#groupNumberOfMembersAndCreatedBy").innerHTML = NumberOfMembersAndCreatedBy;
+                    newGroupList.querySelector(
+                        "#groupNumberOfMembersAndCreatedBy"
+                    ).innerHTML = NumberOfMembersAndCreatedBy;
 
-                    document.getElementById(collection + "-go-here").appendChild(newGroupList);
+                    document
+                        .getElementById(collection + "-go-here")
+                        .appendChild(newGroupList);
                 }
             });
         });
@@ -48,57 +57,57 @@ function writeGroups() {
         name: "Group 1",
         number_of_members: 26,
         created_by: "Maria",
-        is_my_group : true,
+        is_my_group: true,
     });
     groupsRef.add({
         name: "Group 2",
         number_of_members: 44,
         created_by: "Kiana",
-        is_my_group : true,
+        is_my_group: true,
     });
     groupsRef.add({
         name: "Group 3",
         number_of_members: 50,
         created_by: "Shinyoung",
-        is_my_group : true,
+        is_my_group: true,
     });
     groupsRef.add({
         name: "Group 4",
         number_of_members: 27,
         created_by: "Raman",
-        is_my_group : true,
+        is_my_group: true,
     });
     groupsRef.add({
         name: "Group A",
         number_of_members: 30,
         created_by: "Emily",
-        is_my_group : false,
+        is_my_group: false,
     });
     groupsRef.add({
         name: "Group B",
         number_of_members: 33,
         created_by: "Sarah",
-        is_my_group : false,
+        is_my_group: false,
     });
     groupsRef.add({
         name: "Group C",
         number_of_members: 20,
         created_by: "Michael",
-        is_my_group : false,
+        is_my_group: false,
     });
 
     groupsRef.add({
         name: "Group D",
         number_of_members: 45,
         created_by: "Noah",
-        is_my_group : false,
+        is_my_group: false,
     });
 
     groupsRef.add({
         name: "Group E",
         number_of_members: 47,
         created_by: "Benjamin",
-        is_my_group : false,
+        is_my_group: false,
     });
 }
 
